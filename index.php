708ad9cb725f65486cb5e8f2839297a0ea6bbb2e
VidyaBhalodia <vidya.bhalodia@gmail.com>
2016-09-21 06:15:14 -0400

    major functionality done - now make it pretty

--- index.php
+++ index.php
@@ -11,11 +11,15 @@
 
 <form method='POST' action='logic.php'>
     <b> Enter your parameters here : </b> <br>
-    Number of words : <input type='number' name='numberWords' value = 4><br>
-    Choose a separator character  : <input type='text' name='separator'> (leave blank for no separators)<br>
-    <input type="checkbox" name='addNumber' value="Yes"> Add Numbers <br>
-    <input type='text' name='contestant5'><br>
-    <input type='submit' value='Create a password'><br>
+    Number of words : <input type='number' name='numberWords' value = 4 min = 1 max = 9 /> (Max 9 Words) <br>
+    Choose a separator character  : <input type="radio" name="separator" value="" checked="checked" /> None 
+		<input type="radio" name="separator" value="-" /> - Hyphen   
+		<input type="radio" name="separator" value="_" /> _ Underscore <br>     
+    Number of Special Characters : <input type='number' name='numberSpecCharacters' value = 0 min = 0 max = 9 /> (Max 9 Characters) <br>
+<!-- apparently, I need the hidden checkbox or my function complains that I won't send it the right value -->
+	<input type='hidden' value='0' name='addNumber'> 	
+	<input type="checkbox" name='addNumber' value="Yes" /> Add Numbers <br>
+    <input type='submit' value='Create a password' /><br>
 </form>
 
 
--- logic.php
+++ logic.php
@@ -5,24 +5,44 @@
 	/* print_r($dictionaryFile); */ 
 	?>
 
-<?php print_r($_POST); ?> <br>	
-<?php $paramters = $_POST; ?>
-<?php echo $paramters['numberWords']; ?>
+	
+<?php // save the post content as a new variable. 
+$paramters = $_POST; ?>
+
+<?php 
+?>
+
 	
 <?php // generate password - default of 4 words, no spaces
-function makePassword($numberWords, $addInteger) {
+function makePassword($numberWords, $separator, $numberSpecCharacters, $addInteger) {
 	global $dictionaryFile;
 	global $dictionarySize;
 	$passwd = $dictionaryFile[rand(0, $dictionarySize)];
+
+	// create an array with a list of special characters  
+	$specialCharacters =  array("!", "@", "#", "$", "%", "^", "&", "*", "/", "|" ); 
+	
+	// concatenate our word list 
 	for($i = 0; $i < ($numberWords-1) ; $i++) {
-		$passwd = $passwd."-".$dictionaryFile[rand(0, $dictionarySize)];
+		$passwd = $passwd.$separator.$dictionaryFile[rand(0, $dictionarySize)];
 		}
-		
+	// add optional (randomly selected) special characters
+	if ($numberSpecCharacters >= 1) {
+		$passwd = $passwd.$separator;
+		for($i = 0; $i < ($numberSpecCharacters) ; $i++) {
+			$passwd = $passwd.$specialCharacters[rand(0, 9)];
+			}
+		}
+	//	add optional (randomly selected number (0 - 100)
 	if ($addInteger == "Yes") {
-		$passwd = $passwd.'-'.rand(0, 100);
+		$passwd = $passwd.$separator.rand(0, 100);
 		}
 	echo $passwd;
 	}
 	?>
 
-	<?php makePassword($paramters['numberWords'], $paramters['addNumber']); ?>
+	<?php makePassword($paramters['numberWords'],$paramters['separator'], $paramters['numberSpecCharacters'], $paramters['addNumber']); ?>
+
+<form method='POST' action='index.php'>
+    <input type='submit' value='That was fun, I want to do it again !' /><br>
+</form>
